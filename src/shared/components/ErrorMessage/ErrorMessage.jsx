e3180f4b3bb3a251a1c2dd132daeaaf1effa9624
Hlafira <107553024+Hlafira@users.noreply.github.com>
2023-01-29 12:28:07 +0200

    Add project

---
+++ src/shared/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+import styles from './error-message.module.scss';
+const ErrorMessage = ({ message }) => {
+  return <p className={styles.error}>{message}</p>;
+};
+
+export default ErrorMessage;
+
+ErrorMessage.propTypes = {
+  message: PropTypes.string.isRequired,
+};
